b676cfb678df73d8f2cf8cbf74ee5c0df57d9ef9
DarkMatter314 <97723894+DarkMatter314@users.noreply.github.com>
2023-04-06 23:13:02 +0530

    Changed ordering of classes

--- backend/goodeats/models.py
+++ backend/goodeats/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from goodeats.database import db
+from goodeats import db
     
 followers_table = db.Table('Followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
@@ -15,16 +15,20 @@ class User(db.Model):
     password = db.Column(db.String(60), nullable=False)
 
     # Relationships
-    followers = db.Relationship('User', secondary=followers_table,  primaryjoin=(followers_table.c.follower_id == id), 
+    followers = db.relationship('User', secondary=followers_table,  primaryjoin=(followers_table.c.follower_id == id), 
                                 secondaryjoin=(followers_table.c.following_id == id),
-                                backref=db.backref('followers_table', lazy='dynamic'), 
+                                # backref=db.backref('following', lazy='dynamic'), 
+                                lazy='dynamic')
+    following = db.relationship('User', secondary=followers_table,  primaryjoin=(followers_table.c.follower_id == id), 
+                                secondaryjoin=(followers_table.c.following_id == id),
+                                # backref=db.backref('follower', lazy='dynamic'), 
                                 lazy='dynamic')
     recipes = db.relationship('Recipe', backref='author', lazy=True)
     reviews = db.relationship('Reviews', backref='author', lazy=True)
     collections = db.relationship('Collections', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}')"
     
 recipe_keywords = db.Table('recipe_keywords',
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True),
@@ -50,7 +54,7 @@ class Ingredients(db.Model):
     def __repr__(self):
         return f"Ingredient: '{self.keyword}'"  
 
-class Recipe(db.Model): 
+class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     datePublished = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
@@ -75,6 +79,7 @@ class Recipe(db.Model):
     sodium = db.Column(db.Numeric, nullable=True)
 
     #Relationships
+    reviews = db.relationship('Reviews', backref='recipe', lazy=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     keywords = db.relationship('Keywords', secondary=recipe_keywords, lazy='subquery', backref=db.backref('recipes', lazy=True))
     ingredients = db.relationship('Ingredients', secondary=recipe_ingredients, lazy='subquery', backref=db.backref('recipes', lazy=True))
@@ -82,19 +87,38 @@ class Recipe(db.Model):
     def __repr__(self):
         return f"Recipe('{self.title}', '{self.date_posted}') \n '{self.description}'"
     
+    def to_dict(self):
+        return {
+            'name': self.name, 
+            'description': self.description, 
+            'datePublished': self.datePublished,
+            'reviewCount': self.reviewCount, 
+            'avgRating': self.avgRating
+        }
+    
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     datePosted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     rating = db.Column(db.Integer, nullable=False)
     reviewText = db.Column(db.Text, nullable=True)
-    reviewLikes = db.Column(db.Integer, nullable=True)
+    reviewLikes = db.Column(db.Integer, nullable=True, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
 
     def __repr__(self):
         return f"Review: '{self.reviewText}' by '{self.user_id.author}'"
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'datePosted': self.datePosted.isoformat(),
+            'reviewText': self.reviewText,
+            'reviewLikes': self.reviewLikes,
+            'author': self.author
+        }
 
 collection_recipes = db.Table('collection_recipes',
-    db.Column('collection_id', db.Integer, db.ForeignKey('collections.id'), primary_key=True),
+    db.Column('collection_id', db.Integer, db.ForeignKey('collection.id'), primary_key=True),
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
 )
 
@@ -106,6 +130,3 @@ class Collections(db.Model):
 
     def __repr__(self):
         return f"Collection '{self.collectionName}' created by '{self.user_id.author}'"
-    
-
-
