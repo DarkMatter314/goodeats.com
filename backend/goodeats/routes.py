f673ba1510fa6e9ef6f19de95b546428584b22cf
DarkMatter314 <97723894+DarkMatter314@users.noreply.github.com>
2023-04-28 14:11:25 +0530

    Fixed register

--- backend/goodeats/routes.py
+++ backend/goodeats/routes.py
@@ -98,6 +98,7 @@ def register():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, name=form.name.data, email=form.email.data, password=hashed_password, image_file=form.profile_picture.data)
         db.session.add(user)
+        db.session.commit()
         favourite_collection = Collections(collectionName='Favourites', description='Your favourite recipes!', user_id=user.id)
         db.session.add(favourite_collection)
         user.favourites_id = favourite_collection.id
